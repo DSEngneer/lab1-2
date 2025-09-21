1c49a01ec2e0ee7549fd4fa118afaa6c4621f067
ENGENEER <vladyslav.kuderchuk@oa.edu.ua>
2025-09-22 02:03:32 +0300

    lab_2

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Profile from "./Profile";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const users = [
+  {
+    name: "Leanne Graham",
+    role: "Software Engineer",
+    avatarUrl: "https://api.dicebear.com/9.x/lorelei/svg?seed=1",
+  },
+  {
+    name: "Ervin Howell",
+    role: "Frontend Developer",
+    avatarUrl: "https://api.dicebear.com/9.x/lorelei/svg?seed=2",
+  },
+  {
+    name: "Clementine Bauch",
+    role: "UI/UX Designer",
+    avatarUrl: "https://api.dicebear.com/9.x/lorelei/svg?seed=3",
+  },
+  {
+    name: "Patricia Lebsack",
+    role: "Backend Developer",
+    avatarUrl: "https://api.dicebear.com/9.x/lorelei/svg?seed=4",
+  },
+  {
+    name: "Chelsey Dietrich",
+    role: "DevOps Engineer",
+    avatarUrl: "https://api.dicebear.com/9.x/lorelei/svg?seed=5",
+  },
+  {
+    name: "Mrs. Dennis Schulist",
+    role: "QA Engineer",
+    avatarUrl: "https://api.dicebear.com/9.x/lorelei/svg?seed=6",
+  },
+  {
+    name: "Kurtis Weissnat",
+    role: "Product Manager",
+    avatarUrl: "https://api.dicebear.com/9.x/lorelei/svg?seed=7",
+  },
+];
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <h1>User Profiles</h1>
+      <div className="profiles-container">
+        {users.map((user, index) => (
+          <Profile
+            key={index}
+            name={user.name}
+            role={user.role}
+            avatarUrl={user.avatarUrl}
+          />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
